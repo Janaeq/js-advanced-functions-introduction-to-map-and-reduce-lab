115e529f78b64e03fe7dd1e90758a422fb9f4ef9
Janae Quinones <janaemassa@gmail.com>
2021-03-29 14:27:34 -0400

    done

--- index.js
+++ index.js
@@ -1,46 +1,86 @@
 // Your code here
 
-// Map-like methods
+// Map-like methods, not exactly map
 
 function mapToNegativize(arr) {
-    return arr.map(e => e * -1)
+    let newArr = [] // non-destructive changes
+    for (let i = 0; i < arr.length; i++ ) {
+        newArr.push(-1 * arr[i])
+    }
+  return newArr
 }
 
 function mapToNoChange(arr) {
-    return arr.map(e => e)
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i])
+    }
+    return newArr
 }
 
 function mapToDouble(arr) {
-    return arr.map(e => e * 2)
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i] * 2)
+    }
+    return newArr
 }
 
 function mapToSquare(arr) {
-    return arr.map(e => e * e)
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i] * arr[i])
+    }
+    return newArr
 }
 
 // reduce-like methods
 // current value = the element position in the arr
 // accumulator is set to whatever number you want to start at. In this case, it is 'start' passed in as an arg
+// function reduceToTotal(arr, start = 0) {
+//     return arr.reduce((accumulator, currentValue) => {
+//         return accumulator + currentValue
+//     }, start) 
+// }
+
 function reduceToTotal(arr, start = 0) {
-    return arr.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue
-    }, start) 
+    let sum = start
+    for (let e of arr) {
+        sum += e
+    }
+    return sum
 }
+
+// function reduceToAllTrue(arr) {
+//     return arr.reduce((accumulator, currentValue) => {
+//         if (!!currentValue === true)
+//             return !!currentValue
+//         else
+//             return false
+//     }, 0)
+// }
 
 function reduceToAllTrue(arr) {
-    return arr.reduce((accumulator, currentValue) => {
-        if (!!currentValue === true)
-            return !!currentValue
-        else
+    for (let i = 0; i < arr.length; i++) {
+        if (!!arr[i] === false)
             return false
-    }, 0)
+    }
+    return true
 }
 
+// function reduceToAnyTrue(arr) {
+//     return arr.reduce((accumulator, currentValue) => {
+//         if (!!currentValue === true)
+//             return true
+//         else
+//             return false
+//     }, 0)
+// }
+
 function reduceToAnyTrue(arr) {
-    return arr.reduce((accumulator, currentValue) => {
-        if (!!currentValue === true)
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === true)
             return true
-        else
-            return false
-    }, 0)
+    }
+    return false
 }
