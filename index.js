325cec84a71ef0a33e06bdceb83b5a9969c3fbe7
Janae Quinones <janaemassa@gmail.com>
2021-03-29 13:48:52 -0400

    done!!!

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+
+// Map-like methods
+
+function mapToNegativize(arr) {
+    return arr.map(e => e * -1)
+}
+
+function mapToNoChange(arr) {
+    return arr.map(e => e)
+}
+
+function mapToDouble(arr) {
+    return arr.map(e => e * 2)
+}
+
+function mapToSquare(arr) {
+    return arr.map(e => e * e)
+}
+
+// reduce-like methods
+// current value = the element position in the arr
+// accumulator is set to whatever number you want to start at. In this case, it is 'start' passed in as an arg
+function reduceToTotal(arr, start = 0) {
+    return arr.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue
+    }, start) 
+}
+
+function reduceToAllTrue(arr) {
+    return arr.reduce((accumulator, currentValue) => {
+        if (!!currentValue === true)
+            return !!currentValue
+        else
+            return false
+    }, 0)
+}
+
+function reduceToAnyTrue(arr) {
+    return arr.reduce((accumulator, currentValue) => {
+        if (!!currentValue === true)
+            return true
+        else
+            return false
+    }, 0)
+}
